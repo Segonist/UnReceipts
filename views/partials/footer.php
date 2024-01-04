e0ad108964baff97798c63431ea158d172004b49
Segonist <andriinepryntsev@gmail.com>
2024-01-04 11:12:25 +0100

    Move social links images to local assets folder

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -3,9 +3,7 @@
         <p>Made By <span>Segonist</span></p>
     </div>
     <div class="autor-links">
-        <a href="https://github.com/Segonist" target="_blank"><img src="https://skillicons.dev/icons?i=github"
-                alt="github link"></a>
-        <a href="https://discordapp.com/users/491260818139119626" target="_blank"><img
-                src="https://skillicons.dev/icons?i=discord" alt="discord link"></a>
+        <a href="https://github.com/Segonist" target="_blank"><img src="<?= host_path('assets/img/github.svg') ?>" alt="github link"></a>
+        <a href="https://discordapp.com/users/491260818139119626" target="_blank"><img src="<?= host_path('assets/img/discord.svg') ?>" alt="discord link"></a>
     </div>
 </footer>
