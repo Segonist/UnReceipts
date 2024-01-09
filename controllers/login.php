fb9436c64f615e467004f6ae9ddda6f9f8b72a48
Segonist <andriinepryntsev@gmail.com>
2024-01-09 09:22:49 +0100

    yeah

--- controllers/login.php
+++ controllers/login.php
@@ -6,28 +6,28 @@ use Core\Validator;
 $errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db = App::resolve('Core\Database');
-
     $username = $_POST["username"];
     $password = $_POST["password"];
 
     $valid_username = Validator::valid_string(
-        $string = $username,
-        $empty = "Username is required"
+        string: $username,
+        empty: "Username is required"
     );
     if ($valid_username !== true) {
         $errors[] = $valid_username;
     }
 
     $valid_password = Validator::valid_string(
-        $string = $password,
-        $empty = "Password is required"
+        string: $password,
+        empty: "Password is required"
     );
     if ($valid_password !== true) {
         $errors[] = $valid_password;
     }
 
     if (empty($errors)) {
+        $db = App::resolve('Core\Database');
+
         $query = "SELECT * FROM users WHERE username=:username AND password=:password;";
         $user = $db->query($query, ["username" => $username, "password" => $password])->fetch();
 
--- controllers/register.php
+++ controllers/register.php
@@ -6,34 +6,34 @@ use Core\Validator;
 $errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db = App::resolve("Core\Database");
-
     $username = $_POST["username"];
     $password = $_POST["password"];
     $repeat_password = $_POST["repeat_password"];
 
     $valid_username = Validator::valid_string(
-        $string = $username,
-        $empty = "Username is required",
-        $not_in_range = ["min" => 3, "max" => 32, "message" => "Username must be between 3 and 32 characters"],
-        $reg_exps = ["/^[a-zA-Z0-9_]+$/" => "Username must contain only letters, numbers and underscore"]
+        string: $username,
+        empty: "Username is required",
+        not_in_range: ["min" => 3, "max" => 32, "message" => "Username must be between 3 and 32 characters"],
+        reg_exps: ["/^[a-zA-Z0-9_]+$/" => "Username must contain only letters, numbers and underscore"]
     );
     if ($valid_username !== true) {
         $errors[] = $valid_username;
     }
 
     $valid_password = Validator::valid_string(
-        $string = $password,
-        $empty = "Password is required",
-        $not_in_range = ["min" => 8, "max" => 32, "message" => "Password must be between 8 and 32 characters"],
-        $reg_exps = ["/^(?=.*[a-zA-Z])(?=.*\d).+$/" => "Password must contain at least one letter and number"],
-        $equals = [$password, $repeat_password, "message" => "Passwords do not match"]
+        string: $password,
+        empty: "Password is required",
+        not_in_range: ["min" => 8, "max" => 32, "message" => "Password must be between 8 and 32 characters"],
+        reg_exps: ["/^(?=.*[a-zA-Z])(?=.*\d).+$/" => "Password must contain at least one letter and number"],
+        not_equals: [$password, $repeat_password, "message" => "Passwords do not match"]
     );
     if ($valid_password !== true) {
         $errors[] = $valid_password;
     }
 
     if (empty($errors)) {
+        $db = App::resolve("Core\Database");
+
         $query = "SELECT * FROM users WHERE username=:username;";
         $user = $db->query($query, ["username" => $username])->fetch();
 
